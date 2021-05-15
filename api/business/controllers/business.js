d3459baa98ec4691c05c65629f77f34394776973
Asel <peirisasel373@Gmail.com>
2021-05-15 20:16:03 +0530

    Review model

--- api/business/controllers/business.js
+++ api/business/controllers/business.js
@@ -5,4 +5,27 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async review(ctx) {
+        const { id } = ctx.params
+        const { tagId } = ctx.request.body
+        const user = await strapi.plugins['users-permissions'].services.user.fetch({
+            id: ctx.state.user._id,
+          });
+        
+        const tagResponse = await strapi.query('tag').findOne({ id: tagId });
+        if(!tagResponse){
+            ctx.badRequest({'message':'Invalid tag ID'});
+        }
+
+        let updatedUser = user;
+        updatedUser.user_preferences.push(tagResponse);
+
+        console.log(tagResponse)
+        console.log(updatedUser.user_preferences)
+
+        let data = await strapi.query('user', 'users-permissions').update({id:user.id}, updatedUser);
+
+        ctx.send({'message': 'success', 'data': data})
+    }
+};
